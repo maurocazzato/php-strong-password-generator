8d640eb8c7ee48db12dbb8012365e8ebc20a186a
Maurocazzato <maurizzio.125@gmail.com>
2024-01-24 15:52:17 +0100

    commenti alla funzione

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ function generatePassword($length) {
     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?';
     $password = '';
     
-    
+    // applico un minimo e un massimo di caratteri
     if (!is_numeric($length) || $length < 8 || $length > 32) {
         $length = 8;
     }
-
+    // ciclo per iterare casualmente la passw
     for ($i = 0; $i < $length; $i++) {
         $password .= $characters[rand(0, strlen($characters) - 1)];
     }
