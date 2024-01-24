0d008276f9cddadecbbd07d8f483dff52190ffa6
Maurocazzato <maurizzio.125@gmail.com>
2024-01-24 15:56:01 +0100

    scrittura sistemata

--- index.php
+++ index.php
@@ -18,27 +18,25 @@ session_start();
 
 <body>
     <section>
-    <h1>Password Generator</h1>
-    <form action="index.php" method="GET">
-        <label for="password-length"> Lunghezza Password:</label>
-        <input type="number" id="password-length" name="password_length" min="8" max="32" required>
-        <button type="submit">Genera</button>
-    </form>
+        <h1>Password Generator</h1>
+        <form action="index.php" method="GET">
+            <label for="password-length"> Lunghezza Password:</label>
+            <input type="number" id="password-length" name="password_length" min="8" max="32" required>
+            <button type="submit">Genera</button>
+        </form>
 
-    <?php
- 
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-       
-        include 'functions.php';
+        <?php
+    
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            include 'functions.php';
 
-        
-        $password_length = isset($_GET['password_length']) ? (int)$_GET['password_length'] : 8;
+            $password_length = isset($_GET['password_length']) ? (int)$_GET['password_length'] : 8;
 
-        // genera passw 
-        $password = generatePassword($password_length);
-        echo "<p>La tua password: <strong>$password</strong></p>";
-    }
-    ?>
+            // genera passw 
+            $password = generatePassword($password_length);
+            echo "<p>La tua password: <strong>$password</strong></p>";
+        }
+        ?>
     </section>
 </body>
 </html>
